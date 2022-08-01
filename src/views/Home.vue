74051269d7d8dc218fb028b05a259651a69cd26b
oyeProjects <oyeprojects@hotmail.com>
2022-08-01 18:45:41 +0300

    fundementals of animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,24 @@
   </div>
 </template>
 
+
+<!-- 
+  .enter-from , .enter-to , .enter-active :
+    These allows us to control the animation or transition of the elements onto the page.
+
+   .enter-from : That is a class applied to the element before it enters the browser and this is where we set the initial css state of the element.
+   .enter-to : This is a class applied to the element as it enters the browser and this is where we set the final css state of the elements.
+   .enter-active : This is a class that is applied while the element is transitioning from one state to another.
+
+   .leave-from , .leave-to, .leave-active:
+    This is going to be the sates before it exits the page right here.
+
+    Same rules available for 'leave' properties.
+
+    
+
+-->
+
 <script>
 import { ref } from 'vue'
 import Toast from '../components/Toast'
